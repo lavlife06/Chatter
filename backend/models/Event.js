3989c56c9d4e894c2693cb325e0bab5f7db06209
lavlife06 <lav7990192369@gmail.com>
2020-10-25 14:31:09 +0530

    added frontend libraries and basic setup of backend

---
+++ backend/models/Event.js
@@ -0,0 +1,90 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const eventSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'Host',
+  },
+  game: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    data: Buffer,
+    contentType: String,
+    //required: true,
+  },
+  time: {
+    type: Date,
+    // required: true,
+  },
+  entryFee: String,
+  prizepool: Number,
+  teamsize: Number,
+  title: String,
+  gamelink: String,
+  hostedBy: {
+    type: String,
+    required: true,
+  },
+  hostedById: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  registeredteaminfo: [
+    {
+      teammembersinfo: [
+        {
+          user: {
+            type: Schema.Types.ObjectId,
+          },
+          email: {
+            type: String,
+          },
+          name: {
+            type: String,
+          },
+          username: {
+            type: String,
+          },
+          teamLeader: {
+            type: String,
+          },
+          contact: {
+            type: Number,
+          },
+        },
+      ],
+    },
+  ],
+  registeredplayerinfo: [
+    {
+      email: {
+        type: String,
+      },
+      name: {
+        type: String,
+      },
+      contact: {
+        type: Number,
+      },
+      username: {
+        type: String,
+      },
+    },
+  ],
+  contact: Number,
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Event = mongoose.model('event', eventSchema);
+
+module.exports = Event;
---
+++ backend/models/Profile.js
@@ -0,0 +1,141 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const profileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  email: String,
+  DOB: Date,
+  
+  myevents: [
+    {
+      game: {
+        type: String,
+        required: true,
+      },
+      description: {
+        type: String,
+        required: true,
+      },
+      image: {
+        data: Buffer,
+        contentType: String,
+        // required: true,
+      },
+      // time: {
+      //   type: Date,
+      // },
+      entryFee: String,
+      prizepool: Number,
+      teamsize: Number,
+      title: String,
+      gamelink: String,
+      contact: Number,
+      hostedBy: String,
+      hostedById: {
+        type: Schema.Types.ObjectId,
+        required: true,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  // tag: {
+  //   type: String,
+  // },
+  name: {
+    type: String,
+    default: '',
+  },
+  bio: {
+    type: String,
+    default: '',
+  },
+  username: {
+    type: String,
+    unique: true,
+    default: '',
+  },
+  // numoffollowers:{type:Number},
+  // numoffollowing:{type:Number},
+  // followers: [
+  //   {
+  //     user: {
+  //       type: Schema.Types.ObjectId,
+  //       ref: 'User',
+  //     },
+  //   },
+  // ],
+  // following: [
+  //   {
+  //     user: {
+  //       type: Schema.Types.ObjectId,
+  //       ref: 'User',
+  //     },
+  //   },
+  // ],
+  // achievements: [
+  //   {
+  //     eventname: {
+  //       type: String,
+  //     },
+  //     eventdescription: {
+  //       type: String,
+  //     },
+  //   },
+  // ],
+  // profileviewers: {
+  //   type: String,
+  // },
+  // numofviewers:{
+  // type: Number
+  // },
+  // location: {
+  //   type: String,
+  // },
+  gameinterest: {
+    type: [String],
+  },
+  // social: {
+  // youtube: {
+  //   type: String,
+  // },
+  // twitter: {
+  //   type: String,
+  // },
+  // facebook: {
+  //   type: String,
+  // },
+  // linkedin: {
+  //   type: String,
+  // },
+  //   instagram: {
+  //     type: String,
+  //   },
+  // },
+  // Otherlinks like mineown website link or as origanization i have website to share here
+  // platformname can be like mywebsite or other gaming platformnames and its link
+  // otherlinks: [
+  //   {
+  //     platformname: {
+  //       type: String,
+  //     },
+  //     link: {
+  //       type: String,
+  //     },
+  //   },
+  // ],
+  contact: Number,
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Profile = mongoose.model('profile', profileSchema);
+
+module.exports = Profile;
