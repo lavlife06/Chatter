ba7aa1f7dde92b19998a087a558ac321db2b12e8
Lav Senghani <49972875+lavlife06@users.noreply.github.com>
2021-01-04 16:22:07 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-unused-vars */
+import React, { useState, Fragment, useEffect } from "react";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import setAuthToken from "./reduxstuff/utils/setAuthToken";
+import store from "./reduxstuff/store";
+import { loadUser } from "./reduxstuff/actions/auth";
+import Navbar from "./components/Layout/Navbar";
+import Main from "./components/Main";
+import "./App.css";
+import Check from "./components/Layout/Check";
+
+const App = () => {
+  // useEffect(() => {
+  //   if (localStorage.token) {
+  //     setAuthToken(localStorage.token);
+  //     store.dispatch(loadUser());
+  //   }
+  // }, []);
+
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Login} />
+        {/* <Alert /> */}
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Register} />
+          <Route exact path="/check" component={Check} />
+          <Route exact path="/main" component={Main} />
+          {/* <Route exact path="/profile/:id" component={Profile} /> */}
+          {/* <PrivateRoute exact path="/dashboard" component={Dashboard} /> */}
+        </Switch>
+      </Fragment>
+    </Router>
+  );
+};
+
+export default App;
