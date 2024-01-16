965c6ff130f2e514990c224c9cb47c2ebc90869e
lavlife06 <lav7990192369@gmail.com>
2024-01-16 22:02:52 +0530

    removed serving react files from backend

--- server.js
+++ server.js
@@ -28,15 +28,6 @@ require("./routes/login")(app);
 require("./routes/profile")(app);
 require("./routes/room")(app);
 
-if (process.env.NODE_ENV === "production") {
-    // Set static folder
-    app.use(express.static("client/build"));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
-}
-
 const PORT = process.env.PORT || 3001;
 // console.log(PORT);
 const server = app.listen(PORT, () => {
